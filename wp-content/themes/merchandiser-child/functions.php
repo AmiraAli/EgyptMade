df28b0fc6fe273a90701123e1deda17888dd9d21
AmiraAli <amira_ali_marzouk@outlook.com>
2016-12-12 21:16:40 +0200

    translate wc vendors

---
+++ wp-content/themes/merchandiser-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+// register wc vendor translate strings
+if (function_exists('pll_register_string')) {
+
+    pll_register_string('view_your_store', 'View Your Store', 'wc-vendors');
+    pll_register_string('store_settings', 'Store Settings', 'wc-vendors');
+    pll_register_string('add_new_product', 'Add New Product', 'wc-vendors');
+    pll_register_string('edit_products', 'Edit Products', 'wc-vendors');
+
+    pll_register_string('sales_report', 'Sales Report', 'wc-vendors');
+    pll_register_string('product', 'Product', 'wc-vendors');
+    pll_register_string('quantity', 'Quantity', 'wc-vendors');
+    pll_register_string('commission', 'Commission', 'wc-vendors');
+    pll_register_string('rate', 'Rate', 'wc-vendors');
+    pll_register_string('totals', 'Totals', 'wc-vendors');
+    pll_register_string('show_orders', 'Show Orders', 'wc-vendors');
+    pll_register_string('no_sales_period', 'You have no sales during this period', 'wc-vendors');
+    pll_register_string('no_sales_made', 'You haven\'t made any sales yet', 'wc-vendors');
+    
+    pll_register_string('from', 'From:', 'wc-vendors');
+    pll_register_string('to', 'To:', 'wc-vendors');
+    pll_register_string('show', 'Show', 'wc-vendors');
+}
---
+++ wp-content/themes/merchandiser-child/wc-vendors/dashboard/links.php
@@ -0,0 +1,58 @@
+<?php
+$vendor = wp_get_current_user()->user_login;
+$shop_page = esc_url(add_query_arg(array('vendor_shop' => $vendor), get_post_type_archive_link('product')));
+if(function_exists('pll_get_post')){
+    $settings_page= get_permalink(pll_get_post(WC_Vendors::$pv_options->get_option( 'shop_settings_page' )));
+}else{
+   $settings_page= get_permalink( WC_Vendors::$pv_options->get_option( 'shop_settings_page' ) );
+}
+
+?>
+<br>
+<center>
+    <p>
+        <a target="_blank" href="<?php echo $shop_page; ?>" class="button">
+            <?php
+            if (function_exists('pll_e')) {
+                pll_e('View Your Store');
+            } else {
+                echo _e('View Your Store', 'wcvendors');
+            }
+            ?>
+        </a>
+        <a target="_blank" href="<?php echo $settings_page; ?>" class="button">
+            <?php
+            if (function_exists('pll_e')) {
+                pll_e('Store Settings');
+            } else {
+                echo _e('Store Settings', 'wcvendors');
+            }
+            ?>
+        </a>
+
+        <?php if ($can_submit) { ?>
+            <a target="_blank" href="<?php echo $submit_link; ?>" class="button">
+                <?php
+                if (function_exists('pll_e')) {
+                    pll_e('Add New Product');
+                } else {
+                    echo _e('Add New Product', 'wcvendors');
+                }
+                ?>
+            </a>
+            <a target="_blank" href="<?php echo $edit_link; ?>" class="button">
+                <?php
+                if (function_exists('pll_e')) {
+                    pll_e('Edit Products');
+                } else {
+                    echo _e('Edit Products', 'wcvendors');
+                }
+                ?>
+            </a>
+            <?php
+        }
+        do_action('wcvendors_after_links');
+        ?>
+</center>
+
+<hr>
