36df97b2a9e83f7af5ea48d04aa1f8545fbbe872
aya-adel <eng.ayaadel92@yahoo.com>
2016-12-17 23:15:03 +0200

    translate the chart page and part from checkout page

--- wp-content/themes/merchandiser-child/functions.php
+++ wp-content/themes/merchandiser-child/functions.php
@@ -21,7 +21,7 @@ if (function_exists('pll_register_string')) {
     pll_register_string('from', 'From:', 'wc-vendors');
     pll_register_string('to', 'To:', 'wc-vendors');
     pll_register_string('show', 'Show', 'wc-vendors');
-}
+
 
 /**
  * Register shop, single product ,product category
@@ -32,3 +32,28 @@ if (function_exists('pll_register_string')) {
 /**
  * Rgister cart, checkout
  */
+	
+	 pll_register_string('have_a_coupon?', 'Have a coupon?', 'checkout');
+	 //Click here to enter your code
+	  pll_register_string('click_here_to_enter_your_code', 'Click here to enter your code', 'checkout');
+	  //Coupon code
+	   pll_register_string('coupon_code', 'Coupon code', 'checkout');
+	   //Apply Coupon
+	    pll_register_string('apply_Coupon', 'Apply Coupon', 'checkout');
+		//Billing Details
+     pll_register_string('billing_details', 'Billing Details', 'checkout');
+	 //First name
+	  pll_register_string('first_name', 'First Name', 'checkout');
+	  //CART TOTALS
+	   pll_register_string('cart_totals', 'Cart Totals', 'checkout');
+	   //Subtotal
+	    pll_register_string('subtotal', 'Subtotal', 'checkout');
+		//Total
+		 pll_register_string('total', 'Total', 'checkout');
+		 //Proceed to Checkout
+		  pll_register_string('proceed_to_checkout', 'Proceed to Checkout', 'checkout');
+		  //Update Cart
+		   pll_register_string('update_cart', 'Update Cart', 'checkout');
+		   //Additional Information
+		   pll_register_string('additional_information', 'Additional Information', 'checkout');
+}
