a5f782a01361064de594865442d4b89461944f9c
AmiraAli <amira_ali_marzouk@outlook.com>
2016-12-24 02:35:51 +0200

    navigation my accounts

--- wp-content/themes/merchandiser-child/woocommerce/myaccount/navigation.php
+++ wp-content/themes/merchandiser-child/woocommerce/myaccount/navigation.php
@@ -15,22 +15,31 @@
  * @package WooCommerce/Templates
  * @version 2.6.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-do_action( 'woocommerce_before_account_navigation' );
+do_action('woocommerce_before_account_navigation');
 ?>
 
 <nav class="woocommerce-MyAccount-navigation">
-	<ul>
-		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
-				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>"><?php echo esc_html( $label ); ?></a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+    <ul>
+        <?php foreach (wc_get_account_menu_items() as $endpoint => $label) : ?>
+            <li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?>">
+                <?php
+                if (function_exists('pll_get_post')) {
+                    $page_id = url_to_postid(wc_get_account_endpoint_url($endpoint));
+                    $translate_page_url = get_permalink(pll_get_post($page_id));
+                    $url = add_query_arg($endpoint, "", $translate_page_url);
+                    ?>
+                    <a href="<?php echo $url; ?>"><?php echo pll_e($label); ?></a>
+                <?php } else { ?>
+                    <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>"><?php echo esc_html($label); ?></a>
+
+                <?php } ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </nav>
 
-<?php do_action( 'woocommerce_after_account_navigation' ); ?>
+<?php do_action('woocommerce_after_account_navigation'); ?>
