166fb6d89d88515381d7da3885e27530fede03cb
AmiraAli <amira_ali_marzouk@outlook.com>
2017-01-07 23:19:39 +0200

    add script of myaccount menu

--- wp-content/themes/merchandiser-child/js/custom.js
+++ wp-content/themes/merchandiser-child/js/custom.js
@@ -1,6 +1,17 @@
 jQuery(window).load(function () {
-    var elements = jQuery('.vc_row');
-    elements.each(function () {
-        jQuery(this).css("right", jQuery(this).css('left'));
-    });
+    if (jQuery("html").attr('lang') == 'ar') {
+        // home page shift
+        var elements = jQuery('.vc_row');
+        elements.each(function () {
+            jQuery(this).css("right", jQuery(this).css('left'));
+        });
+        // single product shift
+        jQuery("body.single-product .first_col_split , body.single-product .second_col_split").css('float', 'right');
+        var left = jQuery("body.single-product .product_content_wrapper.layout_half .product-thumbnails-vertical-wrapper").css("left");
+        jQuery("body.single-product .product_content_wrapper.layout_half .product-thumbnails-vertical-wrapper").css('right', left);
+        // menu shift
+        var right = jQuery('.site-header .header-wrapper .tools .myaccount-dropdown ul').css('right');
+        jQuery('.site-header .header-wrapper .tools .myaccount-dropdown ul').css('left', right);
+        jQuery('.site-header .header-wrapper .tools .myaccount-dropdown ul').css('right', "auto");
+    }
 });
